65f10ddc4dfa6d36e35ea9889acf1af117f53bb7
Adam Lesniak <adam@lesniak.pl>
2016-08-09 23:11:40 +0100

    Create Friends listing view in my_friends.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   resources :user_stocks, except: [:edit, :update, :show]
+  resources :users, only: [:show]
+  resources :friendship, only: [:show]
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
