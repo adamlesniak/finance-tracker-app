d1ecea01f246b7ac7d4b3a6ac4d977f669c89a82
Adam Lesniak <adam@lesniak.pl>
2016-08-10 22:40:48 +0100

    Creating lookup for searching Users

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,14 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   
-  get 'my_portfolio', to: 'users#my_portfolio'
+  # Portfolio / Stock
+  get 'my_portfolio',  to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
-  get 'my_friends', to: 'users#my_friends'
+  
+  # Friends
+  get  'my_friends',     to: 'users#my_friends'
+  get  'search_friends', to: 'users#search'
+  post 'add_friend',     to: 'users#add_friend'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
